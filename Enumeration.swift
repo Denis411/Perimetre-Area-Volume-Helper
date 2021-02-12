d3d5c7c9b37ad0a597e4291cc3eb07dcef8ec56c
Bradley Kang <bkang@lcs.on.ca>
2021-02-12 11:57:25 -0500

    Added shape list enumeration

---
+++ Enumeration.swift
@@ -0,0 +1,17 @@
+//
+//  Enumeration.swift
+//  Perimetre Area Volume Helper
+//
+//  Created by Brad Kang on 2021-02-12.
+//
+
+import Foundation
+
+enum ShapeList: String {
+    case cylinder = "cylinder"
+    case sphere = "sphere"
+    case cone = "cone"
+    case squarePyramid = "square pyramid"
+    case rectangularPrism = "rectanguar prism"
+    case triangularPrism = "triangular prism"
+}

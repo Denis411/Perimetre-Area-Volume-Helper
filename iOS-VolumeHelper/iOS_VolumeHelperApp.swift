e9fd1f110271c3c7c9b4955099a36b0902ff9df7
Bradley Kang <bkang@lcs.on.ca>
2021-02-12 11:52:10 -0500

    Moved functions to it's own file

---
+++ iOS-VolumeHelper/iOS_VolumeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_VolumeHelperApp.swift
+//  iOS-VolumeHelper
+//
+//  Created by Brad Kang on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_VolumeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
